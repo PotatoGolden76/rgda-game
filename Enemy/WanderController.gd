705fb58ba268bba090b8a4dd31dabe6d741f5c0b
913_gulei_daniel <daniel.ggradinaru@gmail.com>
2023-02-23 19:44:00 +0200

    wander state

---
+++ Enemy/WanderController.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready
+var start_position = global_position
+@onready
+var target_position = global_position
+@export
+var wander_threshold:int = 64;
+@onready
+var timer = $Timer
+@export
+var wander_leniency:float = 0.5;
+
+func update_target_position():
+	var target_vector = Vector2.ZERO
+	
+	while(target_vector.length() < 32): 
+		target_vector = Vector2(randi_range(-wander_threshold, wander_threshold), randi_range(-wander_threshold, wander_threshold))
+	
+	target_position = start_position + target_vector
+
+func _on_timer_timeout():
+	update_target_position() # Replace with function body.
+
+func get_time_left():
+	return timer.time_left
+	
+func start_timer(duration):
+	timer.start(duration)
--- Enemy/_tempSpider/spider.gd
+++ Enemy/_tempSpider/spider.gd
@@ -8,7 +8,9 @@ var FRICTION:float = 2000
 @export
 var ACCELERATION:float = 4000
 @export
-var MAX_SPEED:float = 2000
+var CHASE_MAX_SPEED:float = 2000
+@export
+var WANDER_MAX_SPEED:float = 1000
 
 enum {
 	IDLE, 
@@ -28,41 +30,61 @@ var animationTree:AnimationTree = $AnimationTree
 var animationState = animationTree.get("parameters/playback")
 @onready
 var playerDetectionZone = $PlayerDetectionZone
+@onready
+var wanderController = $WanderController
 
 func _physics_process(delta):
 	velocity = knockback.move_toward(Vector2.ZERO, kb_friction * delta)
 	knockback = velocity
-	move_and_slide()
+	# move_and_slide()
 
 	match state:
 		IDLE:
+			animationTree.set("parameters/Idle/blend_position", last_direction)
+			animationState.travel("Idle")
+				
 			if(velocity != Vector2.ZERO):
 				last_direction = velocity.normalized()
 			
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-			
 			seek_player()
+			
+			if wanderController.get_time_left() == 0:
+				state = WANDER
+				wanderController.start_timer(1)
 		
 		WANDER:
-			pass
+			seek_player()
+			
+			var wander_direction = global_position.direction_to(wanderController.target_position)
+			last_direction = wander_direction
+			
+			if wanderController.get_time_left() == 0 || (global_position - wanderController.target_position).length() <= wanderController.wander_leniency:
+				state = IDLE
+				wanderController.start_timer(1)
+				
+			else:
+				
+				animationTree.set("parameters/Walk/blend_position", wander_direction)
+				animationState.travel("Walk")
+				
+				velocity = velocity.move_toward(wander_direction * WANDER_MAX_SPEED, ACCELERATION * delta)
 		
+			
 		CHASE:
 			var player = playerDetectionZone.player
 			
 			if player != null:
-				var player_direction = (player.global_position - global_position).normalized()
+				var player_direction = global_position.direction_to(player.global_position)
 				last_direction = player_direction
 				
 				animationTree.set("parameters/Walk/blend_position", player_direction)
 				animationState.travel("Walk")
 				
-				velocity = velocity.move_toward(player_direction * MAX_SPEED, ACCELERATION * delta)
+				velocity = velocity.move_toward(player_direction * CHASE_MAX_SPEED, ACCELERATION * delta)
 				
 			else:
 				state = IDLE
-				
-				animationTree.set("parameters/Idle/blend_position", last_direction)
-				animationState.travel("Idle")
 	
 	sprite.flip_h = velocity.x < 0 || last_direction.x < 0
 	move_and_slide()
@@ -73,7 +95,7 @@ func seek_player():
 		state = CHASE
 	
 
-func _on_hurtbox_area_entered(area):
+func _on_hurtbox_area_entered(_area):
 	knockback = Vector2.RIGHT * 300
 	# queue_free()
  
